0bd0ba214c81516603eb4b513b2f608f6113a076
Steven Sousa <silver.steven67@gmail.com>
2024-02-12 15:59:44 -0500

    Problem 334 Solved

--- 334. Increasing Triplet Subsequence.py
+++ 334. Increasing Triplet Subsequence.py
@@ -25,3 +25,14 @@ Constraints:
 
 1 <= nums.length <= 5 * 105
 -231 <= nums[i] <= 231 - 1"""
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = float('inf')
+        for third in nums:
+            if third <= first:
+                first = third
+            elif third <= second:
+                second = third
+            else:
+                return True
+        return False
